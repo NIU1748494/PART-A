9377f500019b4fb2c464b4d91eec4b47cc319716
NIU1748494 <1748494@uab.cat>
2024-12-09 11:45:54 +0100

    tasca1-1

--- tasca1-1.sh
+++ tasca1-1.sh
@@ -8,20 +8,20 @@ echo "******************* LLISTAT D’ACTORS I ACTRIUS PER DATA DEL PREMI. *****
 echo "***** LLISTAT OSCARS A MILLOR ACTOR:"
 for ((i=2;i<"$lineasactor";i++))
 do
-Year=$(head -$i "$actor" |tail -1 | awk -F, 'NF==5{print $2}' )
-Age=$(head -$i "$actor" |tail -1 | awk -F, 'NF==5{print $3}' )
-Name=$(head -$i "$actor" |tail -1 | awk -F, 'NF==5{print $4}')
-Movie=$(head -$i "$actor" |tail -1 | awk -F, 'NF==5{print $5}' )
+Year=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f2)
+	Age=$(awk -F, 'NF==5{print $0}' $actor| head -$i | tail -1 | cut -d, -f3)
+	Actor=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f4)
+	Movie=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f5)
 echo "* $Year , amb $Age anys $Name per $Movie"
 done
 echo "***** LLISTAT OSCARS A MILLOR ACTRIU:"
 for ((i=2;i<$lineasactriz;i++))
 do
-Year=$(head -$i "$actriz" |tail -1 | awk -F, 'NF==5{print $2}' )
-Age=$(head -$i "$actriz" |tail -1 | awk -F, 'NF==5{print $3}' )
-Name=$(head -$i "$actriz" |tail -1 | awk -F, 'NF==5{print $4}')
-Movie=$(head -$i "$actriz" |tail -1 | awk -F, 'NF==5{print $5}' )
-echo "$Year , amb $Age anys $Name per $Movie"
+Year=$(awk -F, 'NF==5{print $0}' $actriz | head -$i | tail -1 | cut -d, -f2)
+	Age=$(awk -F, 'NF==5{print $0}' $actriz| head -$i | tail -1 | cut -d, -f3)
+	Actor=$(awk -F, 'NF==5{print $0}' $actriz | head -$i | tail -1 | cut -d, -f4)
+	Movie=$(awk -F, 'NF==5{print $0}' $actriz | head -$i | tail -1 | cut -d, -f5)
+echo "* $Year , amb $Age anys $Name per $Movie"
 done
 echo "************************** PREMI UNA TECLA PER CONTINUAR *************************"
 read wait
