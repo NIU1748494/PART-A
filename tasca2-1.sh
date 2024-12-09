87f87a4fa309f03a084a5232d1ed65e6541416c6
NIU1748494 <1748494@uab.cat>
2024-12-09 14:19:36 +0100

    fin tasca2-1

--- tasca2-1.sh
+++ tasca2-1.sh
@@ -3,8 +3,9 @@ ainici=$3
 afin=$4
 actor=$1
 actriz=$2
-lineasactor=$(wc -l < "$actor")
-lineasactriz=$(wc -l <"$actriz")
+ano_catalogo=0
+lineasactor=$(wc -l < "$actor")-1
+lineasactriz=$(wc -l <"$actriz")-2
 echo "Actores"
 for ((i=2;i<$lineasactor;i++))
 do
@@ -16,10 +17,11 @@ age=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f3)
 	name=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f4)
 	movie=$(awk -F, 'NF==5{print $0}' $actor | head -$i |tail -1 | cut -d, -f5)
 	echo " $year $age $name $movie " >>ordenar_hombres
+	sort -t, -k5 ordenar_hombres 
+rm ordenar_hombres
 fi
 done
-sort -t, -k5 ordenar_hombres 
-rm ordenar_hombres
+
 echo "Actrices"
 for ((i=2;i<$lineasactriz;i++))
 do
@@ -31,7 +33,13 @@ age=$(awk -F, 'NF==5{print $0}' $actor | head -$i | tail -1 | cut -d, -f3)
 	name=$(awk -F, 'NF==5{print $0}' $actriz | head -$i | tail -1 | cut -d, -f4)
 	movie=$(awk -F, 'NF==5{print $0}' $actriz | head -$i |tail -1 | cut -d, -f5)
 	echo " $year $age $name $movie " >>ordenar_mujeres
+	sort -t, -k5 ordenar_mujeres 
+rm ordenar_mujeres
 fi
 done
-sort -t, -k5 ordenar_mujeres 
-rm ordenar_mujeres
+
+if [ $ano_catalogo -ne 1 ];
+then
+echo "Año no presente en el catalogo "
+fi
+
